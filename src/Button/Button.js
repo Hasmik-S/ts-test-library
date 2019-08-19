066350ec124cadf53444d6a88321f37180098e91
Hasmik <hasmik.sargsyan@t-systems.com>
2019-08-19 15:57:32 +0200

    Button and button-presets changings

--- src/Button/Button.js
+++ src/Button/Button.js
@@ -1,95 +1,81 @@
-import React from 'react';
-import PropTypes from 'prop-types';
-import ButtonM from '@material-ui/core';
+import React from "react";
+import PropTypes from "prop-types";
+import ButtonM from "@material-ui/core/Button";
 
-const Button = props => < ButtonM {...props}/>;
+function Button(props) {
+  return (
+    <ButtonM {...props} onClick={props.onClick}>
+      {props.children}
+    </ButtonM>
+  );
+}
 
+Button.propTypes = {
+  onClick: PropTypes.func,
+  /**
+   * The content of the button.
+   */
+  children: PropTypes.node,
+  /**
+   * Override or extend the styles applied to the component.
+   */
+  classes: PropTypes.object,
+  className: PropTypes.string,
 
-Button.PropTypes = {
+  /**
+   * The color of the component. It supports those theme colors that make sense for this component.
+   */
+  color: PropTypes.oneOf(["default", "inherit", "primary", "secondary"]),
+  /**
+   * If `true`, the button will be disabled.
+   */
+  disabled: PropTypes.bool,
+  /**
+   * If `true`, the  keyboard focus ripple will be disabled.
+   * `disableRipple` must also be true.
+   */
+  disableFocusRipple: PropTypes.bool,
+  /**
+   * If `true`, the ripple effect will be disabled.
+   */
+  disableRipple: PropTypes.bool,
+  focusVisibleClassName: PropTypes.string,
+  /**
+   * If `true`, the button will take up the full width of its container.
+   */
+  fullWidth: PropTypes.bool,
+  /**
+   * The URL to link to when the button is clicked.
+   * If defined, an `a` element will be used as the root node.
+   */
+  href: PropTypes.string,
+  /**
+   * If `true`, and `variant` is `'fab'`, will use mini floating action button styling.
+   */
+  mini: PropTypes.bool,
+  /**
+   * The size of the button.
+   * `small` is equivalent to the dense button styling.
+   */
+  size: PropTypes.oneOf(["small", "medium", "large"]),
+  type: PropTypes.string,
 
-    /**
-         * The content of the button.
-         */
-        children: PropTypes.node.isRequired,
-      
-        /**
-         * Override or extend the styles applied to the component.
-         * See [CSS API](#css) below for more details.
-         */
-        classes: PropTypes.object.isRequired,
-      
-        /**
-         * @ignore
-         */
-        className: PropTypes.string,
-      
-        /**
-         * The color of the component. It supports those theme colors that make sense for this component.
-         */
-        color: PropTypes.oneOf(['default', 'inherit', 'primary', 'secondary']),
-      
-        /**
-         * The component used for the root node.
-         * Either a string to use a DOM element or a component.
-         */
-        component: PropTypes.node,
-      
-        /**
-         * If `true`, the button will be disabled.
-         */
-        disabled: PropTypes.bool,
-      
-        /**
-         * If `true`, the  keyboard focus ripple will be disabled.
-         * `disableRipple` must also be true.
-         */
-        disableFocusRipple: PropTypes.bool,
-      
-        /**
-         * If `true`, the ripple effect will be disabled.
-         *
-         * ⚠️ Without a ripple there is no styling for :focus-visible by default. Be sure
-         * to highlight the element by applying separate styles with the `focusVisibleClassName`.
-         */
-        disableRipple: PropTypes.bool,
-      
-        /**
-         * @ignore
-         */
-        focusVisibleClassName: PropTypes.string,
-      
-        /**
-         * If `true`, the button will take up the full width of its container.
-         */
-        fullWidth: PropTypes.bool,
-      
-        /**
-         * The size of the button.
-         * `small` is equivalent to the dense button styling.
-         */
-        size: PropTypes.oneOf(['small', 'medium', 'large']),
-      
-        /**
-         * @ignore
-         */
-        type: PropTypes.string,
-      
-        /**
-         * The variant to use.
-         */
-        variant: PropTypes.oneOf(['text', 'outlined', 'contained'])
-};
-        
-
-Button.defaultProps = {
-    color: 'primary',
-    variant: 'contained',
-    size: 'medium',
-    disabled: false,
-    disableRipple: false,
-    fullWidth: false,
+  /**
+   * The variant to use.
+   * __WARNING__: `flat` and `raised` are deprecated.
+   * Instead use `text` and `contained` respectively.
+   * `fab` and `extendedFab` are deprecated.
+   * Instead use `<Fab>` and `<Fab variant="extended">`
+   */
+  variant: PropTypes.oneOf([
+    "text",
+    "outlined",
+    "contained",
+    "fab",
+    "extendedFab",
+    "flat",
+    "raised"
+  ])
 };
 
-
-
-export default Button;
+export { Button as default };
--- src/Button/presets/o-default.jsx
+++ src/Button/presets/o-default.jsx
@@ -1,10 +1,13 @@
-import React from 'react';
-import Button from '../Button';
+import React from "react";
+import Button from "../Button.js";
 
 export default (
-
-    <Button uxpId="action0" variant="contained" color="primary">
-        Primary
-    </Button>
-
+  <Button 
+    uxpId="action1" 
+    size="small" 
+    variant="contained" 
+    color="primary" 
+    fullWidth>
+    Sign Up
+  </Button>
 );
